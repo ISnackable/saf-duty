3e7b52ff5ea6ebba6835fb9cd286f26306b59d95
Tommy <52971804+ISnackable@users.noreply.github.com>
2023-02-19 14:31:05 +0800

    feat: add sanity workflow + use env

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -7,8 +7,8 @@ export default defineConfig({
   name: 'default',
   title: 'Duty Roster',
 
-  projectId: 'lryaljny',
-  dataset: 'production',
+  projectId: import.meta.env.SANITY_STUDIO_PROJECT_ID,
+  dataset: import.meta.env.SANITY_STUDIO_DATASET,
 
   plugins: [deskTool(), visionTool()],
 
