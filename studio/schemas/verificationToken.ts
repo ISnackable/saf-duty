c7975b689918bf1689756c0f0cab49d98f96b7eb
Tommy <52971804+ISnackable@users.noreply.github.com>
2023-02-26 17:25:07 +0800

    chore: test deploy workflow

--- studio/schemas/verificationToken.ts
+++ studio/schemas/verificationToken.ts
@@ -1,22 +1,24 @@
-export default {
+import { defineType, defineField } from "sanity";
+
+export default defineType({
   name: 'verification-token',
   title: 'Verification Token',
   type: 'document',
   fields: [
-    {
+    defineField({
       name: 'identifier',
       title: 'Identifier',
       type: 'string',
-    },
-    {
+    }),
+    defineField({
       name: 'token',
       title: 'Token',
       type: 'string',
-    },
-    {
+    }),
+    defineField({
       name: 'expires',
       title: 'Expires',
       type: 'date',
-    },
+    }),
   ],
-}
+})
