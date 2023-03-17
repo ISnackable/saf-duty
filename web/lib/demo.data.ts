dea597de4fad3d863d8fd6b6967dd47a679cd60a
Tommy <52971804+ISnackable@users.noreply.github.com>
2023-03-17 23:30:08 +0800

    chore(web): format files

--- web/lib/demo.data.ts
+++ web/lib/demo.data.ts
@@ -1,17 +1,17 @@
 // All the demo data that used as fallbacks when there's nothing in the dataset yet
 
-import type { User } from "next-auth";
+import type { User } from 'next-auth'
 
-export const title = "Blog.";
+export const title = 'Blog.'
 
-export const description = "Example description.";
+export const description = 'Example description.'
 
 export const users: User[] = [
   {
-    id: "5eda9aac-bfc1-11ed-afa1-0242ac120002",
-    name: "Jun Jie",
+    id: '5eda9aac-bfc1-11ed-afa1-0242ac120002',
+    name: 'Jun Jie',
     image:
-      "https://images.unsplash.com/photo-1624298357597-fd92dfbec01d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80",
+      'https://images.unsplash.com/photo-1624298357597-fd92dfbec01d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80',
     ord: new Date(2024, 1, 1),
     totalDutyDone: 10,
     weekdayPoints: 0,
@@ -20,10 +20,10 @@ export const users: User[] = [
     blockouts: [new Date(2024, 2, 4)],
   },
   {
-    id: "5eda9fca-bfc1-11ed-afa1-0242ac120002",
-    name: "Zhi Hao",
+    id: '5eda9fca-bfc1-11ed-afa1-0242ac120002',
+    name: 'Zhi Hao',
     image:
-      "https://images.unsplash.com/photo-1586297135537-94bc9ba060aa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80",
+      'https://images.unsplash.com/photo-1586297135537-94bc9ba060aa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80',
     ord: new Date(2024, 2, 28),
     totalDutyDone: 12,
     weekdayPoints: 0,
@@ -32,10 +32,10 @@ export const users: User[] = [
     blockouts: [new Date(2024, 2, 13)],
   },
   {
-    id: "5edaa132-bfc1-11ed-afa1-0242ac120002",
-    name: "Jia Hui",
+    id: '5edaa132-bfc1-11ed-afa1-0242ac120002',
+    name: 'Jia Hui',
     image:
-      "https://images.unsplash.com/photo-1632922267756-9b71242b1592?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80",
+      'https://images.unsplash.com/photo-1632922267756-9b71242b1592?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80',
     ord: new Date(2025, 5, 10),
     totalDutyDone: 10,
     weekdayPoints: 1,
@@ -44,10 +44,10 @@ export const users: User[] = [
     blockouts: [],
   },
   {
-    id: "5edaa2cc-bfc1-11ed-afa1-0242ac120002",
-    name: "Jia Hao",
+    id: '5edaa2cc-bfc1-11ed-afa1-0242ac120002',
+    name: 'Jia Hao',
     image:
-      "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80",
+      'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80',
     ord: new Date(2023, 3, 1),
     totalDutyDone: 9,
     weekdayPoints: 1,
@@ -56,10 +56,10 @@ export const users: User[] = [
     blockouts: [],
   },
   {
-    id: "5edaa416-bfc1-11ed-afa1-0242ac120002",
-    name: "Jun Kai",
+    id: '5edaa416-bfc1-11ed-afa1-0242ac120002',
+    name: 'Jun Kai',
     image:
-      "https://images.unsplash.com/photo-1630841539293-bd20634c5d72?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80",
+      'https://images.unsplash.com/photo-1630841539293-bd20634c5d72?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80',
     ord: new Date(2024, 4, 1),
     totalDutyDone: 6,
     weekdayPoints: 1,
@@ -78,10 +78,10 @@ export const users: User[] = [
     ],
   },
   {
-    id: "78c9dc16-bfc1-11ed-afa1-0242ac120002",
-    name: "Wang Li",
+    id: '78c9dc16-bfc1-11ed-afa1-0242ac120002',
+    name: 'Wang Li',
     image:
-      "https://images.unsplash.com/photo-1630841539293-bd20634c5d72?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80",
+      'https://images.unsplash.com/photo-1630841539293-bd20634c5d72?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80',
     ord: new Date(2024, 9, 29),
     totalDutyDone: 0,
     weekdayPoints: -1,
@@ -90,10 +90,10 @@ export const users: User[] = [
     blockouts: [new Date(2024, 2, 1), new Date(2024, 2, 27)],
   },
   {
-    id: "9d658fae-c343-11ed-afa1-0242ac120002",
-    name: "Darryl Koh",
+    id: '9d658fae-c343-11ed-afa1-0242ac120002',
+    name: 'Darryl Koh',
     image:
-      "https://images.unsplash.com/photo-1630841539293-bd20634c5d72?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80",
+      'https://images.unsplash.com/photo-1630841539293-bd20634c5d72?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80',
     ord: new Date(2024, 2, 13),
     totalDutyDone: 0,
     weekdayPoints: 1,
@@ -119,10 +119,10 @@ export const users: User[] = [
     ],
   },
   {
-    id: "a1196c88-c343-11ed-afa1-0242ac120002",
-    name: "Elliot Tan",
+    id: 'a1196c88-c343-11ed-afa1-0242ac120002',
+    name: 'Elliot Tan',
     image:
-      "https://images.unsplash.com/photo-1630841539293-bd20634c5d72?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80",
+      'https://images.unsplash.com/photo-1630841539293-bd20634c5d72?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80',
     ord: new Date(2024, 6, 23),
     totalDutyDone: 0,
     weekdayPoints: 0,
@@ -145,4 +145,4 @@ export const users: User[] = [
       new Date(2024, 2, 25),
     ],
   },
-];
+]
