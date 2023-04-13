1410dfbafb576ee2209a3a72b1c8eb089fe4b1f1
Tommy <52971804+ISnackable@users.noreply.github.com>
2023-04-13 20:16:58 +0800

    chore: fix type error

--- web/pages/login.tsx
+++ web/pages/login.tsx
@@ -255,7 +255,7 @@ export default function AuthenticationForm() {
         <form onSubmit={form.onSubmit(handleSubmit)}>
           {formType === 'login' && (
             <Anchor
-              loading={isSubmitting}
+              disabled={isSubmitting}
               component="button"
               type="button"
               color="dimmed"
