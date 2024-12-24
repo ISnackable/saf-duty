c2d56c91d6f2b4dde6a2ed887ecbf2ca1e2d83d8
Tommy <52971804+ISnackable@users.noreply.github.com>
2024-12-24 21:13:04 +0800

    use nodejs runtime cuz I can't figure out why edge is hanging

--- apps/app/app/(auth)/api/auth/[...all]/route.ts
+++ apps/app/app/(auth)/api/auth/[...all]/route.ts
@@ -1,14 +1,3 @@
 import { auth, toNextJsHandler } from '@repo/auth/server';
-import type { NextRequest } from 'next/server';
 
-export const runtime = 'edge';
-
-const { GET: GET_HANDLER, POST: POST_HANDLER } = toNextJsHandler(auth.handler);
-
-export async function GET(request: NextRequest) {
-  return await GET_HANDLER(request);
-}
-
-export async function POST(request: NextRequest) {
-  return await POST_HANDLER(request);
-}
+export const { GET, POST } = toNextJsHandler(auth.handler);
