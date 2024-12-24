3bc7285f6e18db73fa424dfdfccf7df7a46ccd25
Tommy <52971804+ISnackable@users.noreply.github.com>
2024-12-25 02:20:20 +0800

    chore: docs [skip deploy]

--- packages/database/index.edge.ts
+++ packages/database/index.edge.ts
@@ -1,5 +1,6 @@
 import 'server-only';
 
+// https://github.com/supabase/supabase/blob/master/apps/docs/content/guides/database/connecting-to-postgres/serverless-drivers.mdx
 // https://github.com/nextauthjs/next-auth/issues/10773
 import { env } from '@repo/env';
 import { createPool } from '@vercel/postgres';
@@ -7,7 +8,7 @@ import { createPool } from '@vercel/postgres';
 import { drizzle } from 'drizzle-orm/vercel-postgres';
 import * as schema from './schema';
 
-// Example Supabase pooled connection string (must use Supavisor)
+// Example Supabase pooled connection string, add the following sufix "?workaround=supabase-pooler.vercel" (important)
 const connectionString = env.DATABASE_URL;
 const pool = createPool({
   connectionString,
