9acdab596b9acb8a5f1955d20429d6ead2f12964
Tommy <52971804+ISnackable@users.noreply.github.com>
2024-12-24 21:04:47 +0800

    hopefully fix vercel edge function hanging?

--- apps/app/app/(auth)/api/auth/[...all]/route.ts
+++ apps/app/app/(auth)/api/auth/[...all]/route.ts
@@ -1,5 +1,14 @@
 import { auth, toNextJsHandler } from '@repo/auth/server';
+import type { NextRequest } from 'next/server';
 
 export const runtime = 'edge';
 
-export const { GET, POST } = toNextJsHandler(auth.handler);
+const { GET: GET_HANDLER, POST: POST_HANDLER } = toNextJsHandler(auth.handler);
+
+export async function GET(request: NextRequest) {
+  return await GET_HANDLER(request);
+}
+
+export async function POST(request: NextRequest) {
+  return await POST_HANDLER(request);
+}
