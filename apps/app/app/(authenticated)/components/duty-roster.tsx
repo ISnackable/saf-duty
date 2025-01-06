3f2827d224df4296ab53df0618dc9aa9bd554b28
Tommy Teo <52971804+ISnackable@users.noreply.github.com>
2025-01-06 14:36:40 +0800

    testing z3 working on vercel

--- apps/app/app/(authenticated)/components/duty-roster.tsx
+++ apps/app/app/(authenticated)/components/duty-roster.tsx
@@ -1,24 +1,42 @@
 'use client';
 import { testZ3 } from '@/lib/z3';
+import { Button } from '@repo/design-system/components/ui/button';
 import Script from 'next/script';
 import * as React from 'react';
 export function DutyRoster() {
   const [status, setStatus] = React.useState('loading');
+  const [timeTaken, setTimeTaken] = React.useState(0);
+  const wasCalled = React.useRef(false);
+
+  async function test() {
+    // @ts-ignore
+    if (!global.initZ3) return;
+    const start = performance.now();
+
+    const result = await testZ3();
+
+    const end = performance.now();
+
+    setTimeTaken(end - start);
+  }
 
   // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
   React.useEffect(() => {
-    // @ts-ignore
-    if (!global.initZ3) return;
-    testZ3();
+    if (wasCalled.current) return;
+    wasCalled.current = true;
+
+    test();
   }, [status]);
 
   return (
     <>
       <Script src="/z3-built.js" onLoad={() => setStatus('loaded')} />
 
-      <div className="mx-auto min-h-[350px] max-w-xl items-center justify-center p-10">
-        TEST
+      <div className="mx-auto max-w-xl items-center justify-center p-10">
+        Status: {status}, Time taken: {timeTaken}
       </div>
+
+      <Button onClick={() => test()}>Reload</Button>
     </>
   );
 }
--- apps/app/lib/z3.ts
+++ apps/app/lib/z3.ts
@@ -1,12 +1,20 @@
-import { init } from 'z3-solver';
+// If we ever plan to support Z3 on the edge runtime / cloudflare workers, we need to use the following code
+// https://github.com/cloudflare/worker-emscripten-template/
+// This means that we need to compile our own version of wasm for Z3, and also insitiate the Z3 context
+// in a different way. For now, this npm packages only supports the browser and node.js runtime
+import { type ContextCtor, init } from 'z3-solver';
+
+let Context: ContextCtor;
 
 export async function testZ3() {
   // @ts-ignore
-  if (!global.initZ3 || !window.crossOriginIsolated) {
-    throw new Error('Z3 not initialized');
+  if (!Context || !global.initZ3) {
+    // @ts-ignore
+    const { Context: Module } = await init();
+    Context = Module;
+    return;
   }
 
-  const { Context } = await init();
   const { Solver, Int, And } = Context('main');
 
   const x = Int.const('x');
@@ -15,4 +23,6 @@ export async function testZ3() {
   console.log(solver);
   solver.add(And(x.ge(0), x.le(9)));
   console.log(await solver.check());
+
+  solver.release();
 }
