7a7caf0de116cec900b59246879ced5e4dbac365
Tommy Teo <52971804+ISnackable@users.noreply.github.com>
2025-01-06 15:51:15 +0800

    fix(app): memory leak z3
    - added some clean up just in case
    - the issue is because we are creating context everytime the function is called

--- apps/app/lib/z3.ts
+++ apps/app/lib/z3.ts
@@ -2,20 +2,45 @@
 // https://github.com/cloudflare/worker-emscripten-template/
 // This means that we need to compile our own version of wasm for Z3, and also insitiate the Z3 context
 // in a different way. For now, this npm packages only supports the browser and node.js runtime
-import { type ContextCtor, init } from 'z3-solver';
 
-let Context: ContextCtor;
+// Setting up the Z3 solver in JavaScript is like shooting yourself in the foot, given the sparse documentation available for it.
+// So here are some links that I found useful: (for my future self)
+// https://medium.com/@claudefournier/takuzu-puzzle-assistant-with-react-work-in-progress-part-3-56e4db12e238
+// https://github.com/microsoft/z3guide
+
+import type { Context, Z3HighLevel, Z3LowLevel } from 'z3-solver';
+
+declare global {
+  interface Window {
+    z3Promise: Promise<Z3HighLevel & Z3LowLevel>;
+  } // use any to escape typechecking
+}
+
+export default async function loadZ3() {
+  const z3 = await import('z3-solver');
+
+  // init z3
+  const z3p: Promise<Z3HighLevel & Z3LowLevel> =
+    window.z3Promise ||
+    (() => {
+      // biome-ignore lint/suspicious/noAssignInExpressions: <explanation>
+      return (window.z3Promise = z3.init());
+    })();
+
+  return z3p;
+}
+
+let z3Context: Context<'main'>;
 
 export async function testZ3() {
-  // @ts-ignore
-  if (!Context || !global.initZ3) {
-    // @ts-ignore
-    const { Context: Module } = await init();
-    Context = Module;
-    return;
+  const { Context, em } = await loadZ3();
+
+  if (!z3Context) {
+    console.log('Creating new context');
+    z3Context = Context('main');
   }
 
-  const { Solver, Int, And } = Context('main');
+  const { Solver, Int, And } = z3Context;
 
   const x = Int.const('x');
 
@@ -24,5 +49,8 @@ export async function testZ3() {
   solver.add(And(x.ge(0), x.le(9)));
   console.log(await solver.check());
 
+  // Clean up
+  solver.reset();
   solver.release();
+  em.PThread.terminateAllThreads();
 }
