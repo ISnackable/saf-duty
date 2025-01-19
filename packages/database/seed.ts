e6c0938707af478ce4ccd40c9e28851fc61bd4dc
Tommy Teo <52971804+ISnackable@users.noreply.github.com>
2025-01-20 00:06:52 +0800

    chore(database): improve seed script

--- packages/database/seed.ts
+++ packages/database/seed.ts
@@ -7,6 +7,33 @@ import * as schema from './schema';
 
 const TODAY = new Date();
 const LASTDAY = new Date(TODAY.getFullYear(), TODAY.getMonth() + 1, 0);
+const USER_UUIDS = [
+  demo.id,
+  '21fa547f-a051-4af7-a71d-67bebace76ad',
+  'f8458b80-8d4a-4e86-9e8f-4cdb219ca7b0',
+  '878b146a-7bcf-446d-8f47-71f07de0502a',
+  '8c2750ed-c1c7-41ff-b8ac-47b669ed6e80',
+  '3af4f1e3-2f7e-4ef7-8cb4-6d9344e2bbe8',
+  '2e43fa6c-9bd4-4a6e-95cc-614f1afbe157',
+  '411ebf89-0b91-4758-b555-a915720f8ce2',
+  '1eb1b505-5f54-41e5-ba1d-6a9ebf0c8b90',
+  'a9722408-a0f3-46c6-b5a0-d8c237dadff5',
+];
+const USER_EMAILS = [
+  demo.email,
+  'elizabeth.martin34@example.com',
+  'jacob.smith92@example.com',
+  'samantha.jameson87@example.com',
+  'robert.brown59@example.com',
+  'olivia.wilson75@example.com',
+  'michael.davis66@example.com',
+  'emily.lee29@example.com',
+  'alexander.clark31@example.com',
+  'grace.taylor46@example.com',
+];
+const USER_IMAGES = USER_UUIDS.map((uuid) => {
+  return `https://api.dicebear.com/9.x/adventurer/svg?backgroundColor=c0aede&seed=${uuid}`;
+});
 
 async function main() {
   await reset(database, schema);
@@ -16,11 +43,11 @@ async function main() {
 
   // Seed the database with demo data
   await seed(database, {
-    organizations: schema.organizations,
-    users: schema.users,
-    accounts: schema.accounts,
-    members: schema.members,
-    profiles: schema.profiles,
+    ...schema,
+    pushSubscriptions: {},
+    verifications: {},
+    passkeys: {},
+    sessions: {},
   }).refine((funcs) => ({
     organizations: {
       count: 1,
@@ -41,19 +68,19 @@ async function main() {
       },
     },
     users: {
-      count: 1,
+      count: USER_UUIDS.length,
       columns: {
-        id: funcs.default({
-          defaultValue: demo.id,
+        id: funcs.valuesFromArray({
+          values: USER_UUIDS,
+          isUnique: true,
         }),
-        name: funcs.default({
-          defaultValue: 'demo',
+        image: funcs.valuesFromArray({
+          values: USER_IMAGES,
+          isUnique: true,
         }),
-        image: funcs.default({
-          defaultValue: 'https://api.dicebear.com/9.x/adventurer/svg?seed=demo',
-        }),
-        email: funcs.default({
-          defaultValue: demo.email,
+        email: funcs.valuesFromArray({
+          values: USER_EMAILS,
+          isUnique: true,
         }),
         emailVerified: funcs.default({
           defaultValue: true,
@@ -61,25 +88,24 @@ async function main() {
         initialOrganizationId: funcs.default({
           defaultValue: `${demo.id.slice(0, -1)}c`,
         }),
-        createdAt: funcs.timestamp(),
-        updatedAt: funcs.timestamp(),
-        role: funcs.default({
-          defaultValue: 'admin',
+        banned: funcs.default({
+          defaultValue: false,
         }),
+        role: funcs.default({
+          defaultValue: 'user',
+        }),
+        onboarded: funcs.default({
+          defaultValue: true,
+        }),
+      },
+      with: {
+        accounts: 1,
+        members: 1,
+        profiles: 1,
       },
     },
     accounts: {
-      count: 1,
       columns: {
-        id: funcs.default({
-          defaultValue: `${demo.id.slice(0, -1)}a`,
-        }),
-        userId: funcs.default({
-          defaultValue: demo.id,
-        }),
-        accountId: funcs.default({
-          defaultValue: demo.id,
-        }),
         providerId: funcs.default({
           defaultValue: 'credential',
         }),
@@ -91,69 +117,17 @@ async function main() {
       },
     },
     members: {
-      count: 1,
       columns: {
-        id: funcs.default({
-          defaultValue: `${demo.id.slice(0, -1)}b`,
-        }),
         organizationId: funcs.default({
           defaultValue: `${demo.id.slice(0, -1)}c`,
         }),
-        userId: funcs.default({
-          defaultValue: demo.id,
-        }),
         role: funcs.default({
-          defaultValue: 'owner',
+          defaultValue: 'member',
         }),
         createdAt: funcs.timestamp(),
       },
     },
     profiles: {
-      count: 1,
-      columns: {
-        id: funcs.default({
-          defaultValue: `${demo.id.slice(0, -1)}e`,
-        }),
-        userId: funcs.default({
-          defaultValue: demo.id,
-        }),
-        organizationId: funcs.default({
-          defaultValue: `${demo.id.slice(0, -1)}c`,
-        }),
-        maxBlockouts: funcs.default({
-          defaultValue: 8,
-        }),
-        userSettings: funcs.default({
-          defaultValue: {
-            notify_on_duty_reminder: true,
-            notify_on_swap_requests: true,
-            notify_on_rosters_published: true,
-          },
-        }),
-      },
-    },
-  }));
-
-  // Seed the database with other 'users' in the organization
-  await seed(database, {
-    ...schema,
-    invitations: {},
-    organizations: {},
-    passkeys: {},
-    sessions: {},
-    verifications: {},
-  }).refine((funcs) => ({
-    users: {
-      columns: {
-        initialOrganizationId: funcs.default({
-          defaultValue: `${demo.id.slice(0, -1)}c`,
-        }),
-        role: funcs.default({
-          defaultValue: 'user',
-        }),
-      },
-    },
-    profiles: {
       columns: {
         organizationId: funcs.default({
           defaultValue: `${demo.id.slice(0, -1)}c`,
@@ -168,6 +142,9 @@ async function main() {
             notify_on_rosters_published: true,
           },
         }),
+      },
+      with: {
+        swapRequests: 1,
       },
     },
     rosters: {
@@ -188,16 +165,6 @@ async function main() {
         }),
       },
     },
-    members: {
-      columns: {
-        organizationId: funcs.default({
-          defaultValue: `${demo.id.slice(0, -1)}c`,
-        }),
-        role: funcs.default({
-          defaultValue: 'member',
-        }),
-      },
-    },
     invitations: {
       columns: {
         organizationId: funcs.default({
@@ -211,9 +178,6 @@ async function main() {
           defaultValue: `${demo.id.slice(0, -1)}c`,
         }),
       },
-    },
-    pushSubscriptions: {
-      count: 1,
     },
     swapRequests: {
       columns: {
